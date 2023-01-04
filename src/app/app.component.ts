8db0f8af4341d7ea531044c3c6c7ca65649ac396
Resul KIZMAZ <algast12@gmail.com>
2023-01-04 16:07:31 +0300

    Lesson 3: Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  title = 'ToDo Application';
+  user = 'Resul Kızmaz';
+  list_items = [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinema', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'Kodlama', action: 'No' },
+    { description: 'Akşam Yemeği', action: 'No' },
+  ];
 }
